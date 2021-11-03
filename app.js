9c507844680bb61f907892d01f6738f636596a65
Jarek <jarekpoczta1989@wp.pl>
2021-11-03 18:25:54 +0100

    adding home page

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-require('dotenv').config();
 const http = require('http');
 const fs   = require('fs');
+require('dotenv').config();
 
 const port = process.env.PORT || 3001;
 
 // creating HTTP server
 const httpServer = http.createServer((request, responce) => {
+
+  if(request.url === '/') {
+    fs.readFile('views/home.html', (error, data) => {
+      if(error) {
+        console.log(error);
+      } else {
+        responce.writeHead(200, {'Content-Type': 'text/html'});
+        responce.write(data);
+        responce.end();
+      }
+    });
+  }
 
 });
 httpServer.listen(port, () => {
