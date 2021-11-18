2585db6c197087ca3be813f6e4ba2b0e94a30865
Jarek <jarekpoczta1989@wp.pl>
2021-11-18 19:11:18 +0100

    Creating HTTP server

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-const express = require('express');
-const dotenv  = require('dotenv');
+const http = require('http');
+const fs = require('fs');
+const dotenv  = require('dotenv');//.config({path: './.env'});
+
 
 const port = process.env.PORT || 3001;
 
-// express app
-const app = express();
-// setting the path to environment variables
-dotenv.config({path: './.env'});
+const httpServer = http.createServer((request, response) => {
 
-
-// parse url-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({extended: false}));
-// parse a json bodies (as sent by API clients)
-app.use(express.json());
-// register view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
-
-// listen for requests
-app.listen(3000, () => {
-  console.log(`Server running at port ${port}`);
 });
-
-
-// define routes
-app.get('/', (req, res) => {
-  res.render('./home', {port: port});
+httpServer.listen(port, () => {
+  console.log(`HTTP server listen on ${port}...`);
 });
+// // parse url-encoded bodies (as sent by HTML forms)
+// app.use(express.urlencoded({extended: false}));
+// // parse a json bodies (as sent by API clients)
+// app.use(express.json());
+// // register view engine
+// app.set('view engine', 'ejs');
+// app.set('views', './views');
 
-// 404 page
-app.use((req, res) => {
-  res.status(404).render('./404');
-});
+
+// // listen for requests
+// app.listen(3000, () => {
+//   console.log(`Server running at port ${port}`);
+// });
+
+
+// // define routes
+// app.get('/', (req, res, next) => {
+//   res.render('./home', {port: port});
+// });
+
+// // 404 page
+// app.use((req, res, next) => {
+//   res.status(404).render('./404');
+// });
